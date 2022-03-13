68132463004e04e036cc89f4fba168abe044b323
eiae <erikignasi@gmail.com>
2022-03-13 19:28:15 +0100

    add HD plot section

--- model_charts/Plot_IRF_FEVD_HD.py
+++ model_charts/Plot_IRF_FEVD_HD.py
@@ -36,8 +36,8 @@ res = ["IRF", "FEVD", "HD"]
 res_bear = ["IRF", "FEVD", "hist decomp"]
 cc = ["BR", "MX"]
 country_long = ["Brazil", "Mexico"]
-colors = ["g", "b"]  #, "k", "m", "c", "r", "y", "k"]
-palettes = ["YlGn", "PuBu"]  #, "bone_r", "RdPu", "BrBG", "YlOrRd"]
+colors = ["g", "b"] 
+palettes = ["Greens", "Blues"] 
 
 xls = {}
 data = {}
@@ -159,7 +159,7 @@ bar_width = 0.8
 delim = 32  # adapt values to isolate correct string from label
 
 for i, k in zip(cc, palettes):
-    col_pal[i] = cm.get_cmap(k, v)
+    col_pal[i] = cm.get_cmap(k, v+1)  # avoid white in cmap
     for row in range(v):
         alphas.append(1.0-(1/v*row))  # alphas equal to number of variables
         FEVD_median[str(row+1)] = []
@@ -182,7 +182,7 @@ for i, k in zip(cc, palettes):
             lbl = label["FEVD"][i]["FEVD_"+str(row+1)+"_"+str(col+1)]
             ax.bar(bar1, FEVD_plot[str(row+1)][col],
                    bottom=np.sum(FEVD_plot[str(row+1)][:col], axis=0),
-                   width=bar_width, label=lbl[delim:], color=col_pal[i](col),  # delim
+                   width=bar_width, label=lbl[delim:], color=col_pal[i](col+1),  # delim
                    edgecolor="k", linewidth=0.3) 
 
         ax.tick_params(axis="both", labelsize=18)
@@ -203,107 +203,62 @@ for i, k in zip(cc, palettes):
 
 # Plot HD
 # =============================================================================
+HD_median = {}
+HD_total = {}
+HD_plot = {}
+HD_cumulated_data = {}
+HD_cumulated_data_neg = {}
+HD_row_mask = {}
+HD_stacked_data = {}
 
+col_pal = {}
 
-#-----------------------------------------------------------------------------
-# appendix
-#-----------------------------------------------------------------------------
+bar_width = 0.8
 
-# preamble countries
-#--------------------
-# colours = ["g", "b", "k", "m", "c", "r"]
-# palettes = ["YlGn", "PuBu", "bone_r", "RdPu", "BrBG", "YlOrRd"]
-# country = ["ID", "IN", "KR", "MY", "PH", "TH"]
-# country_long = ["indonesia", "india", "korea", "malaysia", "philippines", "thailand"]
+def get_cumulated_array(data, **kwargs):
+    cum = data.clip(**kwargs)
+    cum = np.cumsum(cum, axis=0)
+    df = np.zeros(np.shape(data))
+    df[1:] = cum[:-1]
+    return df
 
-# preamble FEVD and HD
-#----------------------
-# FEVD_median = {}
-# FEVD_total = {}
-# FEVD_rel_median = {}
-# FEVD_plot = {}
+for i, k in zip(cc, palettes):
+    col_pal[i] = cm.get_cmap(k, v+1)  # avoid white in cmap
+    for row in range(v):
+        HD_median[str(row+1)] = []
+        for col in range(v):
+            median = output["HD"][i]["HD_"+str(row+1)+"_"+str(col+1)]["median"]
+            bar2 = [i for i in range(len(median))]
+            tick2 = [i for i in bar2]
+            HD_median[str(row+1)].append(median)
+        HD_total[str(row+1)] = list(map(sum, zip(*HD_median[str(row+1)])))
+        HD_plot[str(row+1)] = np.zeros([v, t-1])
+        for col in range(v):
+            HD_plot[str(row+1)][col, :] = HD_median[str(row+1)][col].values
+        HD_cumulated_data[str(row+1)] = get_cumulated_array(HD_plot[str(row+1)], min=0)
+        HD_cumulated_data_neg[str(row+1)] = get_cumulated_array(HD_plot[str(row+1)], max=0)
+        HD_row_mask[str(row+1)] = (HD_plot[str(row+1)] < 0)
+        HD_cumulated_data[str(row+1)][HD_row_mask[str(row+1)]] = HD_cumulated_data_neg[str(row+1)][HD_row_mask[str(row+1)]]
+        HD_stacked_data[str(row+1)] = HD_cumulated_data[str(row+1)]
 
-# HD_median = {}
-# HD_total = {}
-# HD_plot = {}
-# HD_cumulated_data = {}
-# HD_cumulated_data_neg = {}
-# HD_row_mask = {}
-# HD_stacked_data = {}
+        fig, ax = plt.subplots(1, figsize=(15, 7))
+        for col in range(v):
+            lbl = label["HD"][i]["HD_"+str(row+1)+"_"+str(col+1)]
+            ax.bar(bar2, HD_plot[str(row+1)][col],
+                   bottom=HD_stacked_data[str(row+1)][col],
+                   width=bar_width, label=lbl[16:-20],  # adapt values to isolate correct string from label
+                   color=col_pal[i](col+1), edgecolor="k", linewidth=0.1)
+        ax.plot(bar2, HD_total[str(row+1)], color="k", linewidth=5, label="actual")
 
-# bar_width = 0.8
-# alphas = []
-# for i in range(v):
-#     alphas.append(1.0-(1/v*i))  # alphas equal to number of variables
-# col_pal = {}
-# for i in range(len(country)):
-#     col_pal["palette"+"_"+country[i]] = cm.get_cmap(palettes[i], v)
+        ax.tick_params(axis="both", labelsize=18)
+        plt.xticks(np.arange(0, len(tick2), 4), [median.index[j][:-2] for j in np.arange(0, len(tick2), 4)], rotation='vertical')
+        plt.xlim([min(tick2)-bar_width, max(tick2)+bar_width])
+        ax.patch.set_facecolor("white")
+        ax.grid(color="k", alpha=0.2, linewidth=0.5, linestyle="--")
 
-# def get_cumulated_array(data, **kwargs):
-#     cum = data.clip(**kwargs)
-#     cum = np.cumsum(cum, axis=0)
-#     df = np.zeros(np.shape(data))
-#     df[1:] = cum[:-1]
-#     return df
+        ax.set_title("HD:" + lbl[32:-12], fontsize=20)  # adapt values to isolate correct string from label
+        ax.set_ylabel("contribution to fluctuation", fontsize=18)
+        # ax.set_xlabel("sample")
+        ax.legend(loc="center left", fontsize="x-large", bbox_to_anchor=(1, 0.6), frameon=False)
 
-# plotting FEVD and HD
-#----------------------
-# for row in range(v):
-    #     # FEVD
-    #     FEVD_median["FEVD_median"+"_"+str(row+1)] = []
-    #     for col in range(v):
-    #         bar_l_1 = [i+1 for i in range(len(output["output_"+res[1]]["output_"+res[1]+"_"+str(country[i])][res[1]+"_"+str(country[i])+"_"+str(row+1)+"_"+str(col+1)]["median"]))]
-    #         tick_pos_1 = [i for i in bar_l_1]
-    #         FEVD_median["FEVD_median"+"_"+str(row+1)].append(output["output_"+res[1]]["output_"+res[1]+"_"+str(country[i])][res[1]+"_"+str(country[i])+"_"+str(row+1)+"_"+str(col+1)]["median"])
-    #     FEVD_total["FEVD_total"+"_"+str(row+1)] = list(map(sum, zip(*FEVD_median["FEVD_median"+"_"+str(row+1)])))
-    #     for col in range(v):
-    #         FEVD_rel_median["FEVD_rel_median"+"_"+str(row+1)+"_"+str(col+1)] = (FEVD_median["FEVD_median"+"_"+str(row+1)][col] / FEVD_total["FEVD_total"+"_"+str(row+1)])*100
-    #     FEVD_plot["FEVD_plot"+"_"+str(row+1)] = np.zeros([v, r-2])
-    #     for col in range(v):
-    #         FEVD_plot["FEVD_plot"+"_"+str(row+1)][col, :] = FEVD_rel_median["FEVD_rel_median_"+str(row+1)+"_"+str(col+1)].values
-    #     fig, ax = plt.subplots(1, figsize=(15, 7))
-    #     for col in range(v):
-    #         ax.bar(bar_l_1, FEVD_plot["FEVD_plot"+"_"+str(row+1)][col], bottom=np.sum(FEVD_plot["FEVD_plot"+"_"+str(row+1)][:col], axis=0),  width=bar_width, label="contribution of "+label["label_"+res[1]]["label_"+res[1]+"_"+str(country[i])][res[1]+"_"+str(country[i])+"_"+str(row+1)+"_"+str(col+1)][36:], color=col_pal["palette"+"_"+str(country[i])](col), edgecolor="k", linewidth=0.3) # alpha=alphas[col],
-    #     ax.tick_params(axis="both", labelsize=18) ###
-    #     plt.xticks(tick_pos_1, bar_l_1)
-    #     plt.xlim([min(tick_pos_1)-bar_width, max(tick_pos_1)+bar_width])
-    #     plt.ylim(0, 100)
-    #     ax.patch.set_facecolor("white")
-    #     ax.legend(loc="center left", fontsize="x-large", bbox_to_anchor=(1, 0.6), frameon=False)
-    #     ax.set_ylabel("Variability of "+label["label_"+res[1]]["label_"+res[1]+"_"+str(country[i])][res[1]+"_"+str(country[i])+"_"+str(row+1)+"_"+str(1)][8:-34], fontsize=18)
-    #     #ax.set_xlabel("horizon")
-    #     #ax.set_ylabel("contribution in %")
-    #     #ax.set_title(res[1]+": "+label["label_"+res[1]]["label_"+res[1]+"_"+str(country[i])][res[1]+"_"+str(country[i])+"_"+str(row+1)+"_"+str(1)][8:-35])  # positions to isolate title from string in labels
-    #     fig.savefig(os.path.join(OUTPUTS["OUTPUTS_"+str(country[i])], res[1]+"_"+str(country[i])+"_"+str(row+1)+".png"), dpi=200, bbox_inches="tight")
-    #     fig.savefig(os.path.join(OUTPUTS["OUTPUTS_"+str(country[i])], res[1]+"_"+str(country[i])+"_"+str(row+1)+".pdf"), dpi=200, bbox_inches="tight")
-    # for row in range(v):
-    #     # HD
-    #     HD_median["HD_median"+"_"+str(row+1)] = []
-    #     for col in range(v):
-    #         bar_l_2 = [i for i in range(len(output["output_"+res[2]]["output_"+res[2]+"_"+str(country[i])][res[2]+"_"+str(country[i])+"_"+str(row+1)+"_"+str(col+1)]["median"]))]
-    #         tick_pos_2 = [i for i in bar_l_2]
-    #         HD_median["HD_median"+"_"+str(row+1)].append(output["output_"+res[2]]["output_"+res[2]+"_"+str(country[i])][res[2]+"_"+str(country[i])+"_"+str(row+1)+"_"+str(col+1)]["median"])
-    #     HD_total["HD_total"+"_"+str(row+1)] = list(map(sum, zip(*HD_median["HD_median"+"_"+str(row+1)])))
-    #     HD_plot["HD_plot"+"_"+str(row+1)] = np.zeros([v, r_2-2])
-    #     for col in range(v):
-    #         HD_plot["HD_plot"+"_"+str(row+1)][col, :] = HD_median["HD_median_"+str(row+1)][col].values
-    #     HD_cumulated_data["HD_cumulated"+"_"+str(row+1)] = get_cumulated_array(HD_plot["HD_plot"+"_"+str(row+1)], min=0)
-    #     HD_cumulated_data_neg["HD_cumulated_neg"+"_"+str(row+1)] = get_cumulated_array(HD_plot["HD_plot"+"_"+str(row+1)], max=0)
-    #     HD_row_mask["HD_row_mask"+"_"+str(row+1)] = (HD_plot["HD_plot"+"_"+str(row+1)] < 0)
-    #     HD_cumulated_data["HD_cumulated"+"_"+str(row+1)][HD_row_mask["HD_row_mask"+"_"+str(row+1)]] = HD_cumulated_data_neg["HD_cumulated_neg"+"_"+str(row+1)][HD_row_mask["HD_row_mask"+"_"+str(row+1)]]
-    #     HD_stacked_data["HD_stacked_data"+"_"+str(row+1)] = HD_cumulated_data["HD_cumulated"+"_"+str(row+1)]
-    #     fig, ax = plt.subplots(1, figsize=(15, 7))
-    #     for col in range(v):
-    #         ax.bar(bar_l_2, HD_plot["HD_plot"+"_"+str(row+1)][col], bottom=HD_stacked_data["HD_stacked_data"+"_"+str(row+1)][col],  width=bar_width, label=label["label_"+res[2]]["label_"+res[2]+"_"+str(country[i])][res[2]+"_"+str(country[i])+"_"+str(row+1)+"_"+str(col+1)][:-24], color=col_pal["palette"+"_"+str(country[i])](col), edgecolor="k", linewidth=0.1) # alpha=alphas[col],
-    #     ax.plot(bar_l_2, HD_total["HD_total"+"_"+str(row+1)], color="k", linewidth=5, label="actual")
-    #     ax.tick_params(axis="both", labelsize=18) ###
-    #     plt.xticks(np.arange(0, len(tick_pos_2), 4), [output["output_"+res[2]]["output_"+res[2]+"_"+str(country[i])][res[2]+"_"+str(country[i])+"_"+str(row+1)+"_"+str(col+1)].index[j][:-2] for j in np.arange(0, len(tick_pos_2), 4)], rotation='vertical')
-    #     plt.xlim([min(tick_pos_2)-bar_width, max(tick_pos_2)+bar_width])
-    #     ax.patch.set_facecolor("white")
-    #     ax.grid(color="k", alpha=0.2, linewidth=0.5, linestyle="--")
-    #     ax.legend(loc="center left", fontsize="x-large", bbox_to_anchor=(1, 0.6), frameon=False)
-    #     ax.set_ylabel("Fluctuations of "+label["label_"+res[2]]["label_"+res[2]+"_"+str(country[i])][res[2]+"_"+str(country[i])+"_"+str(row+1)+"_"+str(1)][35:-11], fontsize=18)
-    #     #ax.set_ylabel("contribution to fluctuation")
-    #     #ax.set_title(res[2]+": "+label["label_"+res[2]]["label_"+res[2]+"_"+str(country[i])][res[2]+"_"+str(country[i])+"_"+str(row+1)+"_"+str(1)][35:-11])  # positions to isolate title from string in labels
-    #     fig.savefig(os.path.join(OUTPUTS["OUTPUTS_"+str(country[i])], res[2]+"_"+str(country[i])+"_"+str(row+1)+".png"), dpi=200, bbox_inches="tight")
-    #     fig.savefig(os.path.join(OUTPUTS["OUTPUTS_"+str(country[i])], res[2]+"_"+str(country[i])+"_"+str(row+1)+".pdf"), dpi=200, bbox_inches="tight")
+        fig.savefig(os.path.join(OUTPUTS[i], "HD_"+i+"_"+str(row+1)+".png"), dpi=200, bbox_inches="tight")
